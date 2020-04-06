ba26ae5c7b46511cc38213234e16ac5d2ffb9012
joniberchtold <54633221+joniberchtold@users.noreply.github.com>
2020-04-06 15:31:04 +0200

    Suchfunktion

--- resultate.php
+++ resultate.php
@@ -36,24 +36,27 @@
                 die;
             }else{
             
-                $sql = "SELECT ID, Name, Artikelnummer, Bild, Beschreibung FROM artikel WHERE Name LIKE '%$suchstring%'";
+                $sql = "SELECT ProductID, Fahrzeugname, Einsatzgebiet, Bild, Name_des_Halters, Telefonnummer, Email FROM fahrzeuge WHERE Fahrzeugname LIKE '%$suchstring%'";
                 
                 $count_var = 0;
                 
-        foreach ($pdo->query($sql) as $row) {
-                $produkt_id = $row['ID'];
-				$produkt_name = $row['Name'];
-				$produkt_artikelnummer = $row['Artikelnummer'];
-                $produkt_bild = $row['Bild'];
-                $produkt_beschreibung = $row['Beschreibung'];
-				$count_var++;
+				foreach ($pdo->query($sql) as $row) {
+					$produkt_id = $row['ProductID'];
+					$produkt_name = $row['Fahrzeugname'];
+					$produkt_gebiet = $row['Einsatzgebiet'];
+					$produkt_bild = $row['Bild'];
+					$produkt_halter = $row['Name_des_Halters'];
+					$produkt_telefon = $row['Telefonnummer'];
+					$produkt_email = $row['Email'];
+					$count_var++;
     ?>
-            <div id="produkt">
+			<div id="produkt">
             <div id="produkt_titel"><h1><?php echo $produkt_name; ?></h1></div>
-            <div id="produkt_bild"><img src="bilder/<?php echo $produkt_bild; ?>" /></div>
-			<div id="produkt_nummer"><?php echo $produkt_artikelnummer; ?></div>
-            <div id="produkt_beschreibung"><?php echo $produkt_beschreibung; ?></div>
-            
+            <div id="produkt_bild"><img src="bild/<?php echo $produkt_bild; ?>" width="300px;"/></div>
+			<div id="produkt_gebiet"><?php echo $produkt_gebiet; ?></div>
+            <div id="produkt_halter"><?php echo $produkt_halter; ?></div>
+			<div id="produkt_telefon"><?php echo $produkt_telefon; ?></div>
+			<div id="produkt_email"><?php echo $produkt_email; ?></div>
 
     <?php
         }
